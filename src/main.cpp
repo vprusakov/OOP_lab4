a7836d72c37354d41db42050ecf78b0c9cc53668
Вячеслав Прусаков <vyacheslav.prusakov@gmail.com>
2017-11-22 14:35:20 +0300

    done

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,15 @@
 #include "PrimeFactors.h"
 
 int main(int argc, char* argv[]) {
-	if (argc < 3) {
+
+	if (argc != 3) {
 		std::cerr << "Arguments error" << std::endl;
 		return 0;
 	}
 
 	std::ifstream fin(argv[1]);
 	std::ofstream fout(argv[2]);
+
 
 	if (!(fin.is_open() && fout.is_open())) {
 		std::cerr << "Can't open files." << std::endl;
@@ -18,14 +20,15 @@ int main(int argc, char* argv[]) {
 
 	uint64_t cur_value;
 	while (fin >> cur_value) {
-		PrimeFactors pf(cur_value);
-		fout << cur_value << " = " << pf.ToString() << std::endl;
+		if (cur_value <= UINT64_MAX && cur_value > 0) {
+			PrimeFactors pf(cur_value);
+			fout << cur_value << " = " << pf.ToString() << std::endl;
+		}
 	}
 
 	fout << std::endl;
 
 	fin.close();
 	fout.close();
-
 	return 0;
 }
